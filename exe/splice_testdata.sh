fea1d60df8fb2ffb74eaa1c4e8f55b7497f74776
Hugues Fontenelle <hugues.fontenelle@gmail.com>
2015-04-28 11:57:31 +0200

    [vcpipe][splice] #17 pyvcf

--- exe/splice_testdata.sh
+++ exe/splice_testdata.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
-FILES="../data/case_study0.vcf
-../data/case_study1.vcf
-../data/case_study2.vcf
-../data/case_study3.vcf
-../data/de_novo.vcf"
+set -euf -o pipefail
 
-REFSEQ="../../../../vcpipe-bundle/genomic/gatkBundle_2.5/human_g1k_v37_decoy.fasta"
-REFSEQGENE="../../../../vcpipe-bundle/funcAnnot/refseq/refGene_131119.tab"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+FILES="${DIR}/../data/case_study0.vcf"
+${DIR}/../data/case_study1.vcf
+${DIR}/../data/case_study2.vcf
+${DIR}/../data/case_study3.vcf
+${DIR}/../data/de_novo.vcf"
+
+REFSEQ="${DIR}/../../../../../vcpipe-bundle/genomic/gatkBundle_2.5/human_g1k_v37_decoy.fasta"
+REFSEQGENE="${DIR}/../../../../../vcpipe-bundle/funcAnnot/refseq/refGene_131119.tab"
+
+export PYTHONPATH="${DIR}/.."
 
 for f in $FILES
 do
 	# annotate the VCF file using Jian14nar (default) method
-	python ../splice/splice_annotate.py -i $f --refseqgene $REFSEQGENE --refseq $REFSEQ
+	python ${DIR}/../splice/splice_annotate.py -i $f --refseqgene $REFSEQGENE --refseq $REFSEQ
 	# Optional: create JSON output for all methods:
-	python ../splice/splice_score.py -i $f --all --refseqgene $REFSEQGENE --refseq $REFSEQ
-	python ../splice/splice_predict.py -i ${f%.*}.json --method Jian14nar --refseqgene $REFSEQGENE --refseq $REFSEQ
-	python ../splice/splice_predict.py -i ${f%.*}.json --method Houdayer --refseqgene $REFSEQGENE --refseq $REFSEQ
-	python ../splice/splice_predict.py -i ${f%.*}.json --method AMG-diag --refseqgene $REFSEQGENE --refseq $REFSEQ
-	python ../splice/splice_predict.py -i ${f%.*}.json --method AMG-kreftgenetikk --refseqgene $REFSEQGENE --refseq $REFSEQ
+	#python ${DIR}/../splice/splice_score.py -i $f --all --refseqgene $REFSEQGENE --refseq $REFSEQ
+	#python ${DIR}/../splice/splice_predict.py -i ${f%.*}.json --method Jian14nar --refseqgene $REFSEQGENE --refseq $REFSEQ
+	#python ${DIR}/../splice/splice_predict.py -i ${f%.*}.json --method Houdayer --refseqgene $REFSEQGENE --refseq $REFSEQ
+	#python ${DIR}/../splice/splice_predict.py -i ${f%.*}.json --method AMG-diag --refseqgene $REFSEQGENE --refseq $REFSEQ
+	#python ${DIR}/../splice/splice_predict.py -i ${f%.*}.json --method AMG-kreftgenetikk --refseqgene $REFSEQGENE --refseq $REFSEQ
 done
-
